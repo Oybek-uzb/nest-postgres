c67538c5205ebecafef187eedb662c3bdcd40e85
Oybek-uzb <programmingiswonderful@gmail.com>
2022-04-15 07:53:59 +0500

    CRUD is ready

--- src/feed/controllers/feed.controller.ts
+++ src/feed/controllers/feed.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateDateColumn, UpdateResult } from 'typeorm';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
 
 @Controller('feed')
-export class FeedController {}
+export class FeedController {
+    constructor(private feedService: FeedService){}
+
+    @Post()
+    create(@Body() post: FeedPost): Observable<FeedPost> {
+        return this.feedService.createPost(post)
+    }
+
+    @Get()
+    getAll(): Observable<FeedPost[]> {
+        return this.feedService.findAllPosts()
+    }
+
+    @Put(':id')
+    update(
+        @Body() post: FeedPost, 
+        @Param('id') id: number
+    ): Observable<UpdateResult> {
+        return this.feedService.updatePost(post, id)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number): Observable<DeleteResult> {
+        return this.feedService.deletePost(id)
+    }
+}
--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedController } from './controllers/feed.controller';
+import { FeedPostEntity } from './models/post.entity';
 import { FeedService } from './services/feed.service';
 
 @Module({
-  providers: [FeedService]
+  imports: [
+    TypeOrmModule.forFeature([FeedPostEntity])
+  ],
+  providers: [FeedService],
+  controllers: [FeedController]
 })
 export class FeedModule {}
